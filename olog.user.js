861294bbdeb6afe762b250ee0ba9ec0c93cd6128
skiwi <frank_skiwi@hotmail.com>
2015-10-29 22:03:05 +0100

    Changed tabs to 4 spaces

--- olog.user.js
+++ olog.user.js
@@ -2,29 +2,29 @@
 // @name                OLog
 // @namespace	        http://www.olog.com/
 // @description	        OLog Userscript
-// @include		http://s135-en.ogame.gameforge.com/game/*
+// @include	            http://s135-en.ogame.gameforge.com/game/*
 // ==/UserScript==
 
 var page = getQueryVariable("page");
 
 if (page === "messages") {
-	var observer = new MutationObserver(function(mutations) {
-		mutations.forEach(function(mutation) {
-			for (var node of mutation.addedNodes) {
-				if (node.localName === "ul" && node.classList.contains("tab_inner")) {
-					for (var messageNode of node.querySelectorAll(".msg")) {
-						processMessageNode(messageNode);
-					}
-				}
-			}
-		});
-	});
-	
-	observer.observe(document, { childList: true, subtree: true });
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            for (var node of mutation.addedNodes) {
+                if (node.localName === "ul" && node.classList.contains("tab_inner")) {
+                    for (var messageNode of node.querySelectorAll(".msg")) {
+                        processMessageNode(messageNode);
+                    }
+                }
+            }
+        });
+    });
+    
+    observer.observe(document, { childList: true, subtree: true });
 }
 
 function processMessageNode(node) {
-	console.log(node);
+    console.log(node);
 }
 
 function getQueryVariable(variable) {
